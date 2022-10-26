cf61a29edf9b20621043d0a51780d84ce0b3f286
Gilvan Oliveira Silva <115108137+GilvanOSilva@users.noreply.github.com>
2022-10-26 18:24:01 -0300

    Novo dimensionamento de campos: letras maiores, campos com pre-inscrição de valores e alinhamento de palavras

--- calc_poup.py
+++ calc_poup.py
@@ -4,8 +4,10 @@ from tkinter import *
 
 def valor_total():
     rendimento = entry_rendimento.get()
+    rendimento = rendimento.replace(',', '.')
     rendimento = float(rendimento)
     valor = entry_valor.get()
+    valor = valor.replace(',', '.')
     valor = float(valor)
 
     valor_rend = (valor * rendimento) / 100
@@ -21,34 +23,57 @@ def valor_total():
 
 locale.setlocale(locale.LC_ALL, '')
 
-# modo teste
-# valor = 49000.00
-# rendimento = 0.6790
-
 screen = Tk()
+# screen.geometry('550x200')
+padvar = 3
+fontsize = 15
 screen.title('Cálculo de poupança')
 
-label_valor = Label(screen, text='Valor a calcular.')
-label_valor.grid(column=0, row=0)
+label_valor = Label(screen, text='Valor a calcular', padx=padvar, pady=padvar, justify='left', font=fontsize)
+label_valor.grid(column=0, row=0, sticky=(W))
+
 
 valor = DoubleVar()
-entry_valor = Entry(screen, textvariable=valor)
+valor.set('0000,00')
+entry_valor = Entry(screen, textvariable=valor, font=fontsize)
 entry_valor.grid(column=1, row=0)
 
-label_rendimento = Label(screen, text='Valor do rendimento.')
-label_rendimento.grid(column=0, row=1)
+label_rendimento = Label(screen, text='Valor do rendimento', padx=padvar, pady=padvar, font=fontsize)
+label_rendimento.grid(column=0, row=1, sticky=(W))
+
+
 
 rendimento = DoubleVar()
-entry_rendimento = Entry(screen, textvariable=rendimento)
+rendimento.set('0,0000')
+entry_rendimento = Entry(screen, textvariable=rendimento, font=fontsize)
 entry_rendimento.grid(column=1, row=1)
 
-button_calcular = Button(screen, text='Calcular valor reajustado', command=valor_total)
-button_calcular.grid(column=0, row=2)
+label_data = Label(screen, text='Data do rendimento', padx=padvar, pady=padvar, font=fontsize)
+label_data.grid(column=0, row=2, sticky=(W))
 
-label_valor_rend = Label(screen, text='')
-label_valor_rend.grid(column=0, row=3)
+data_rend = DoubleVar()
+data_rend.set('00/00/0000')
+entry_data = Entry(screen, textvariable=data_rend, font=fontsize)
+entry_data.grid(column=1, row=2)
 
-label_total = Label(screen, text='')
-label_total.grid(column=0, row=4)
+button_calcular = Button(screen, text='Calcular valor reajustado', command=valor_total, padx=padvar, pady=padvar, font=fontsize)
+button_calcular.grid(column=2, row=1, sticky=(W))
+
+button_calcular = Button(screen, text='Calcular valor pela data', command=valor_total, padx=padvar, pady=padvar, font=fontsize)
+button_calcular.grid(column=2, row=2, sticky=(W, E))
+
+title_valor_rend = Label(screen, text='Rendimento', padx=padvar, pady=padvar, font=fontsize)
+title_valor_rend.grid(column=0, row=4, sticky=(E))
+
+label_valor_rend = Label(screen, text='', padx=padvar, pady=padvar, relief='sunken', font=fontsize)
+label_valor_rend.grid(column=1, row=4, sticky=(W, E))
+
+title_total = Label(screen, text='Valor total', font=fontsize)
+title_total.grid(column=0, row=5, sticky=(E))
+
+
+label_total = Label(screen, text='', padx=padvar, pady=padvar, relief='sunken', font=fontsize)
+label_total.grid(column=1, row=5, sticky=(W, E))
+
 
 screen.mainloop()
