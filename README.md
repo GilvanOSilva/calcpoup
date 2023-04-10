c60d1dc541c97f0776a6991dd143de33ba0eba94
Gilvan Oliveira Silva <115108137+GilvanOSilva@users.noreply.github.com>
2023-04-10 17:55:05 -0300

    Documentado arquivo README

--- README.md
+++ README.md
@@ -1,5 +1,48 @@
-# calcpoup
-Cálculo de poupança
+CALCPOUP
 
-Sobre o calcpoup
-calcpoup é um programa que calcula o rendimento da poupança informando o valor a ser reajustado e o fator para reajuste do valor da conta poupança, retornando o valor reajustado com o rendimento.
+Sobre o programa
+----------------
+
+Calcpoup é um programa que efetua o cálculo de reajuste de um valor de uma poupança, sendo possível calcular
+com base na data de reajuste(dia do depósito na poupança) ou o valor direto do rendimento.
+
+Características
+---------------
+
+O programa efetua em sua inicialização a obtenção dos valores de reajuste em modo automático, sendo necessário
+informar o saldo a ser reajustado(saldo da conta poupança) e a data de reajuste(dia do depósito na conta 
+poupança), mas também é possível calcular o informando o valor do reajuste diário(o valor do reajuste informado 
+pelo Banco Central do Brasil).
+
+Sistema com interface gráfica;
+Reajuste da poupança com base na data de reajuste;
+Reajuste da poupança pelo valor do reajuste;
+Exibe o saldo reajustado e o valor do rendimento;
+
+Funções não efetuadas pelo programa
+-----------------------------------
+
+consultoria de investimento.
+
+Instalação
+----------
+
+Para utilização do programa é necessária a instalação do interpretador python3 para sua execução, no site
+https://www.python.org/downloads/. Todas as versões do python3 são suportadas.
+
+Notas Técnicas
+--------------
+Uso do tkinter para interface gráfica.
+Uso do scrappy para obtenção automática do rendimentos da poupança no site do Banco Central do Brasil.
+
+Modificação
+-----------
+
+O propósito deste programa é demonstrar a obtenção e utilização automática de dados para cálculo do rendimento de 
+poupança, dispensando a consulta no site do Banco Central do Brasil, sua modificação está liberada dentro das
+regras de licença GNU. 
+
+Funções adicionais
+------------------
+Em atualizações futuras o programa será disponibilizado uma versão de arquivo executável dispensando instalação do 
+python3, porem esta versão não apresenta o código fonte.
--- calc_poup.py
+++ calc_poup.py
@@ -3,29 +3,29 @@ from tkinter import *
 from ScrapyBcb.BcPoup.spiders import Bcb
 
 
-def valor_total():
+def valor_total(flag=0):
     rendimento = entry_rendimento.get()
     rendimento = rendimento.replace(',', '.')
     rendimento = float(rendimento)
     valor = entry_valor.get()
     valor = valor.replace(',', '.')
     valor = float(valor)
-
     valor_rend = (valor * rendimento) / 100
     total = valor_rend + valor
     valor_rend = locale.currency(valor_rend, grouping=True)
-    print('valor do rendimento:', valor_rend)
+    # print('valor do rendimento:', valor_rend)
     total = locale.currency(total, grouping=True)
-    print('Valor total com reajuste:', total)
-
+    # print('Valor total com reajuste:', total)
     label_valor_rend['text'] = valor_rend
     label_total['text'] = total
+    if flag == 0:
+        label_estado['text'] = ' '
 
 
 def buscar_rend():
     label_valor_rend['text'] = ''
     label_total['text'] = ''
-    lista = open('D:/pys/pycharm/calcpoup/ScrapyBcb/BcPoup/spiders/rendimento.txt', 'r')
+    lista = open('D:/pys/pycharm/calcpoup/rendimento.txt', 'r')
     l_rend = []
     for i in lista:
         l_rend.append(i[:-1])
@@ -35,65 +35,50 @@ def buscar_rend():
         if l_rend[i] == data_rend:
             rendimento.set(l_rend[i+1])
             label_estado['text'] = "Data localizada!"
-            valor_total()
+            valor_total(1)
             break
         if i == len(l_rend)-1:
             label_estado['text'] = "Data não localizada!"
 
 
 locale.setlocale(locale.LC_ALL, '')
-
 # Inicializa o scrapy do rendimentos com fonte o Banco central do Brasil
 Bcb
-
 screen = Tk()
 padvar = 3
 fontsize = 15
 screen.title('Cálculo de poupança')
-
 label_valor = Label(screen, text='Valor a calcular', padx=padvar, pady=padvar, justify='left', font=fontsize)
-label_valor.grid(column=0, row=0, sticky=(W))
-
+label_valor.grid(column=0, row=0, sticky='w')
 valor = DoubleVar()
 valor.set('0000,00')
 entry_valor = Entry(screen, textvariable=valor, font=fontsize)
 entry_valor.grid(column=1, row=0)
-
 label_rendimento = Label(screen, text='Valor do rendimento', padx=padvar, pady=padvar, font=fontsize)
-label_rendimento.grid(column=0, row=1, sticky=(W))
-
+label_rendimento.grid(column=0, row=1, sticky='w')
 rendimento = DoubleVar()
 rendimento.set('0,0000')
 entry_rendimento = Entry(screen, textvariable=rendimento, font=fontsize)
 entry_rendimento.grid(column=1, row=1)
-
 label_data = Label(screen, text='Data do rendimento', padx=padvar, pady=padvar, font=fontsize)
 label_data.grid(column=0, row=2, sticky=(W))
-
 data_rend = DoubleVar()
 data_rend.set('00/00/0000')
 entry_data = Entry(screen, textvariable=data_rend, font=fontsize)
 entry_data.grid(column=1, row=2)
-
-button_calcular = Button(screen, text='Calcular valor reajustado', command=valor_total, padx=padvar, pady=padvar, font=fontsize)
-button_calcular.grid(column=2, row=1, sticky=(W))
-
+button_calcular = Button(screen, text='Calcular valor', command=valor_total, padx=padvar, pady=padvar, font=fontsize)
+button_calcular.grid(column=2, row=1, sticky='w')
 button_data = Button(screen, text='Calcular valor pela data', command=buscar_rend, padx=padvar, pady=padvar, font=fontsize)
-button_data.grid(column=2, row=2, sticky=(W, E))
-
+button_data.grid(column=2, row=2, sticky='we')
 label_estado = Label(screen, text='', padx=padvar, pady=padvar, relief='sunken', font=fontsize)
-label_estado.grid(column=2, rowspan=4, sticky=(N, S, W, E))
-
+label_estado.grid(column=2, rowspan=4, sticky='nsew')
 title_valor_rend = Label(screen, text='Rendimento', padx=padvar, pady=padvar, font=fontsize)
-title_valor_rend.grid(column=0, row=4, sticky=(E))
-
+title_valor_rend.grid(column=0, row=4, sticky='e')
 label_valor_rend = Label(screen, text='', padx=padvar, pady=padvar, relief='sunken', font=fontsize)
-label_valor_rend.grid(column=1, row=4, sticky=(W, E))
-
+label_valor_rend.grid(column=1, row=4, sticky='we')
 title_total = Label(screen, text='Valor total', font=fontsize)
-title_total.grid(column=0, row=5, sticky=(E))
-
+title_total.grid(column=0, row=5, sticky='e')
 label_total = Label(screen, text='', padx=padvar, pady=padvar, relief='sunken', font=fontsize)
-label_total.grid(column=1, row=5, sticky=(W, E))
+label_total.grid(column=1, row=5, sticky='we')
 
 screen.mainloop()
